078967e3354655bf2de54d208a9e58867337926e
Brindha-sree <srikannan547@gmail.com>
2023-05-06 22:31:49 +0530

    script

---
+++ script.js
@@ -0,0 +1,18 @@
+let card = document.getElementById("card");
+let url = "https://api.thecatapi.com/v1/images/search";
+let data = async () => (await fetch(url)).json();
+let btn = document.getElementById("show_more");
+
+btn.onclick = () =>{
+    data().then((res) => {
+        let img = document.createElement("img");
+        img.src = res[0].url;
+        card.appendChild(img)
+
+    }).catch((err) => {
+        alert(err);
+
+    });
+}
+
+
